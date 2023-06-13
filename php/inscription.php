437960a5dcc0c490c27210e4c73fa55ef4d729a1
Antoine Duchesne <antduchesne@hotmail.fr>
2023-06-13 12:06:09 +0200

    fini inscription

---
+++ php/inscription.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="../css/style-inscription.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <div class="inner_header">
+            <div class="logo_container">
+                <a href="../index.html"><img id="image" src="../images/logo_temporaire" alt="LOGO"></a>
+            </div>
+        </div>
+    </header>
+    <div class="login-form">
+            <?php 
+                if(isset($_GET['reg_err']))
+                {
+                    $err = htmlspecialchars($_GET['reg_err']);
+
+                    switch($err)
+                    {
+                        case 'success':
+                        ?>
+                            <div class="alert alert-success">
+                                <strong>Succès</strong> inscription réussie !
+                            </div>
+                        <?php
+                        break;
+
+                        case 'password':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe différent
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email non valide
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email_length':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email trop long
+                            </div>
+                        <?php 
+                        break;
+
+                        case 'name_length':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> pseudo trop long
+                            </div>
+                        <?php
+                        case 'first_name_length':
+                        ?>
+                            <div class="alert alert-danger">
+                            <strong>Erreur</strong> pseudo trop long
+                            </div>
+                        <?php  
+                        case 'already':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> compte deja existant
+                            </div>
+                        <?php 
+
+                    }
+                }
+            ?>
+    <div class="form">
+        <form action="inscription_traitement.php" method="post">
+            <div class="form-group">
+                <label for="first_name">Nom</label>
+                <input type="text" name="first_name" class="form-control" placeholder="Nom" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Prénom</label>
+                <input type="text" name="name" class="form-control" placeholder="Prénom" required>
+            </div>              
+            <div class="form-group">
+                <label for="gender">Genre</label>
+                <input type="radio" name="gender" value="Homme" required>Homme
+                <input type="radio" name="gender" value="Femme" required>Femme
+            </div>
+            <div class="form-group">
+                <label for="birth">Date de naissance</label>
+                <input type="date" name="birth" id="form_birth" required>
+            </div>
+            <div class="form-group">
+                <label for="country">Pays</label>
+                <input type="text" name="country" class="form-control" placeholder="Pays" required>
+            </div>
+            <div class="form-group">
+                <label for="postal">Code postal</label>
+                <input type="text" name="postal" class="form-control" placeholder="95570" required>
+            </div>
+            <div class="form-group">
+                <label for="adress">Adresse</label>
+                <input type="text" name="adress" class="form-control" placeholder="Adresse" required>
+            </div>
+            <div class="form-group">
+                <label for="mail">Adresse mail</label>
+                <input type="email" name="mail" class="form-control" placeholder="Adresse mail" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" class="form-control" placeholder="Mot de passe" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required autocomplete="off">
+            </div>
+            <div class="form-group d-none">
+                <label for="favoris">Favoris</label>
+                <input type="text" name="favoris" class="form-control" placeholder="Favoris">
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" name="interets[]" value="Immobilier">
+                <label class="form-check-label" for="inlineCheckbox1">Immobilier</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" name="interets[]" value="Automobile">
+                <label class="form-check-label" for="inlineCheckbox1">Automobile</label>
+            </div>
+            <div class="form-group">
+            <button type="submit" name="submit" role="button" aria-disabled="false" class="btn">Envoyer</button>
+            <input type="Reset" name="reset" value="Réinitialiser" class="btn">
+            </div>
+        </form>
+    </div>
+
+    <script src="./js/formulaire_inscription.js"></script>
+    <style>
+        body{
+        background-color: #333333;
+        min-width: 1400px;
+        color: white;
+        font-family: 'Roboto', sans-serif;
+        }
+        .login-form {
+            width: 70%;
+            height: 100%;
+            margin-left: 15%;
+        }
+        .login-form form {
+            margin-bottom: 15px;
+            background: #333333;
+            padding: 30px;
+        }
+        .form-control, .btn {
+            min-height: 38px;
+            border-radius: 2px;
+        }
+        .btn {        
+            font-size: 15px;
+            font-weight: bold;
+            background: #006B6B;
+        }
+        </style>
+</body>
+</html>
---
+++ php/inscription_traitement.php
@@ -0,0 +1,68 @@
+<?php
+    require_once 'config.php';
+
+    if(!empty($_POST['first_name']) && !empty($_POST['name']) && !empty($_POST['gender']) && !empty($_POST['birth']) && !empty($_POST['country']) && !empty($_POST['postal']) && !empty($_POST['adress']) && !empty($_POST['mail']) && !empty($_POST['password']) && !empty($_POST['password_retype']))
+    {
+        $first_name = htmlspecialchars($_POST['first_name']);
+        $name = htmlspecialchars($_POST['name']);
+        $gender = htmlspecialchars($_POST['gender']);
+        $birth = htmlspecialchars($_POST['birth']);
+        $country = htmlspecialchars($_POST['country']);
+        $postal = htmlspecialchars($_POST['postal']);
+        $adresse = htmlspecialchars($_POST['adress']);
+        $mail = htmlspecialchars($_POST['mail']);
+        $password = htmlspecialchars($_POST['password']);
+        $password_retype = htmlspecialchars($_POST['password_retype']);
+        $interets = $_POST['interets'];
+
+        $check = $bdd->prepare('SELECT prenom, nom, password FROM utilisateurs WHERE email = ?');
+        $check->execute(array($mail));
+        $data = $check->fetch();
+        $row = $check->rowCount();
+
+        $email = strtolower($mail);
+
+        if($row == 0){
+            if($password == $password_retype){
+                if(strlen($first_name) <= 20){
+                    if(strlen($name) <= 20){
+                        if(strlen($mail) <= 100){
+                            if(filter_var($mail, FILTER_VALIDATE_EMAIL)){
+    
+                                $cost = ['cost' => 12];
+                                $password = password_hash($password, PASSWORD_BCRYPT, $cost);
+    
+                                $ip = $_SERVER['REMOTE_ADDR'];
+
+                                $chk="";  
+                                foreach($interets as $chk1)  
+                                {  
+                                    $chk .= $chk1.",";  
+                                }
+    
+                                $insert = $bdd->prepare('INSERT INTO utilisateurs(email, password, ip, prenom, nom, genre, date_de_naissance, pays, adresse, code_postal, favoris, interets, token) VALUES(:email, :password, :ip, :prenom, :nom, :genre, :date_de_naissance, :pays, :adresse, :code_postal, :favoris, :interets, :token)');
+                                $insert->execute(array(
+                                    'email' => $mail,
+                                    'password' => $password,
+                                    'ip' => $ip,
+                                    'prenom' => $first_name,
+                                    'nom' => $name,
+                                    'genre' => $gender,
+                                    'date_de_naissance' => $birth,
+                                    'pays' => $country,
+                                    'adresse' => $adresse,
+                                    'code_postal' => $postal,
+                                    'favoris' => "",
+                                    'interets' => $chk,
+                                    'token' => bin2hex(openssl_random_pseudo_bytes(64))
+                                ));
+    
+                                header('Location:inscription.php?reg_err=success');
+                                die();
+                            }else {header('Location:inscription.php?reg_err=email'); die();}
+                        }else {header('Location:inscription.php?reg_err=email_length'); die();}
+                    }else {header('Location:inscription.php?reg_err=name_length'); die();}
+                }else {header('Location:inscription.php?reg_err=first_name_length'); die();}
+            }else {header('Location:inscription.php?reg_err=password'); die();}
+        }else {header('Location:inscription.php?reg_err=already'); die();}
+    }
