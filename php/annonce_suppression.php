01f94f9de4cbacabd565e7964f73176757da95c5
Amine KHEDDAOUI <amine.kheddaoui@gmail.com>
2023-06-15 19:53:09 +0200

    a

---
+++ php/annonce_suppression.php
@@ -0,0 +1,8 @@
+<?php
+    require_once "config.php";
+    $id_annonce = $_POST['id_annonce'];
+    $delete = $bdd->prepare('DELETE FROM annonce  WHERE id_annonce = :id_annonce;');
+    $delete->execute(array(
+        'id_annonce'=>$id_annonce
+    ));
+?>
