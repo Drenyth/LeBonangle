58828bc766e37a2a5cd7a1e5058ec0c44ec91301
Laeky <33467433+Laeky@users.noreply.github.com>
2023-06-20 15:35:51 +0200

    Annonce surpression
    Mis à a jour sur mes annonces, gestion d'erreur et annonce suppresion

--- php/annonce_suppression.php
+++ php/annonce_suppression.php
@@ -1,10 +1,27 @@
 <?php
     require_once "config.php";
-    $id_annonce = $_POST['id_annonce'];
+    if( $id_annonce = $_GET['id']){
     $delete = $bdd->prepare('DELETE FROM annonce  WHERE id_annonce = :id_annonce;');
     $delete->execute(array(
         'id_annonce'=>$id_annonce
     ));
+    $request = $bdd->prepare('SELECT * FROM bien WHERE id_annonce = ?');
+    $request->execute(array($id_annonce));
+    $bien = $request->fetch();
+    $row = $request->rowCount();
 
+    if(($row != 0)){
+        $check1 = $bdd->prepare('DELETE FROM bien WHERE id_annonce = ?');
+        $check1->execute(array($id_annonce));
+    }
+    else{
+        $check1 = $bdd->prepare('DELETE FROM service WHERE id_annonce = ?');
+        $check1->execute(array($id_annonce));
+    }
+    header('Location:mes_annonces.php?reg_err=success');die();
+}
+else {
+    header('Location:mes_annonces.php?reg_err=failure');die();
+}
     
 ?>
